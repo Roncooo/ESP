40ef3113684fc01df9ba7fb620cf6c25d0dfcabc
Roncooo <francescooroncolatoo@gmail.com>
2024-05-06 15:49:15 +0200

    Created database WordRoomDatabase.kt (will be modified)

---
+++ codelab-android-room-with-a-view_start/codelab-android-room-with-a-view/app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.android.roomwordssample
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Word::class], version = 1, exportSchema = false)
+public abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    // The companion object makes the variable and the function static
+    companion object {
+
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
