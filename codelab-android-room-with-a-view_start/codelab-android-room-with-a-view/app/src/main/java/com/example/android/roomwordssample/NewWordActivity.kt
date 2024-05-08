d2c2dab89121f365489d9fc2245161b780d10c2c
Roncooo <francescooroncolatoo@gmail.com>
2024-05-08 13:51:16 +0200

    Created NewWordActivity

---
+++ codelab-android-room-with-a-view_start/codelab-android-room-with-a-view/app/src/main/java/com/example/android/roomwordssample/NewWordActivity.kt
@@ -0,0 +1,39 @@
+package com.example.android.roomwordssample
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class NewWordActivity : AppCompatActivity() {
+
+    private lateinit var editWordView: EditText
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+        val editWordView = findViewById<EditText>(R.id.edit_word)
+
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editWordView.text)) {
+                // questa activity restituisce un valore al chiamante
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = editWordView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    // public quindi è una costante nota anche al chiamante
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+}
