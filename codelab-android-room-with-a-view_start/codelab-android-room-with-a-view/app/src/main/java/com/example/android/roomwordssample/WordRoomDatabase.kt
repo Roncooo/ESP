732fc10c2c0b1866a730f7a673cda63920ee33a9
Roncooo <francescooroncolatoo@gmail.com>
2024-05-08 13:39:12 +0200

    A mess just to add some default values in the database

--- codelab-android-room-with-a-view_start/codelab-android-room-with-a-view/app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.kt
+++ codelab-android-room-with-a-view_start/codelab-android-room-with-a-view/app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 public abstract class WordRoomDatabase : RoomDatabase() {
@@ -16,7 +20,10 @@ public abstract class WordRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): WordRoomDatabase {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
@@ -24,11 +31,52 @@ public abstract class WordRoomDatabase : RoomDatabase() {
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()       // quando parte il db non deve salvare i dati della versione precedente
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 // return instance
                 instance
             }
         }
     }
+
+
+    // serve solo per chiamare popuateDatabase all'interno di una coroutine
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            // If you want to keep the data through app restarts,
+            // comment out the following line.
+            INSTANCE?.let { database ->
+                scope.launch(Dispatchers.IO) {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        // Questo serve per la prima volta che eseguo
+        suspend fun populateDatabase(wordDao: WordDao) {
+            // Delete all content here.
+            wordDao.deleteAll()
+
+            // Add sample words.
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+            word = Word("Pizza")
+            wordDao.insert(word)
+            word = Word("Pasta")
+            wordDao.insert(word)
+        }
+    }
+
+
 }
+
+
--- codelab-android-room-with-a-view_start/codelab-android-room-with-a-view/app/src/main/java/com/example/android/roomwordssample/WordsApplication.kt
+++ codelab-android-room-with-a-view_start/codelab-android-room-with-a-view/app/src/main/java/com/example/android/roomwordssample/WordsApplication.kt
@@ -1,11 +1,14 @@
 package com.example.android.roomwordssample
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordsApplication : Application() {
     // anche per le val si può specificare una inizializzazione lazy
     // (come nel design pattern proxy) l'oggetto viene istanziato solo quando è veramente necessario
     // in questo caso particolare sarà istanziato subito lo stesso perché deve far partire il recycler view
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
