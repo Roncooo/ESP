e96f2c3b4b074e17b6042a22bb5af0cf5873b88d
Roncooo <francescooroncolatoo@gmail.com>
2024-05-28 08:50:04 +0200

    Added instrumented test with espresso (division by zero)

--- SimpleCalcTest_start/app/src/androidTest/java/com/example/android/simplecalctest/MainActivityInstrumentedTest.kt
+++ SimpleCalcTest_start/app/src/androidTest/java/com/example/android/simplecalctest/MainActivityInstrumentedTest.kt
@@ -35,4 +35,19 @@ class MainActivityInstrumentedTest {
 		
 	}
 	
+	@Test
+	fun errorDivisionByZero() {
+		// ora le varie text view o edit text non sono più nello spazio di indirizzamento della funzione
+		// ma sono nell'emulatore quindi devo andarle a pescare in un modo che non è più findViewById
+		
+		onView(withId(R.id.operand_one_edit_text)).perform(typeText("1.0"), closeSoftKeyboard())
+		onView(withId(R.id.operand_two_edit_text)).perform(typeText("0.0"), closeSoftKeyboard())
+		// click in espresso è un oggetto che rappresenta l'azione quindi diciamo tipo design pattern messenger
+		// c'è un pezzo di espresso nel mio computer e un pezzo nel dispositivo (perché l'abbiamo scritto in gradle)
+		onView(withId(R.id.operation_div_btn)).perform(click())
+		
+		// recupero il risultato e faccio l'equivalente di una assert
+		onView(withId(R.id.operation_result_text_view)).check(matches(withText("Error")))
+		
+	}
 }
